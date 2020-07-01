60eb7772cd9c1d45fe856142b7d747dd0063734e
Nick Heiner <nheiner@netflix.com>
2020-07-01 14:31:30 -0700

    Adding test

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,3 +1,30 @@
-test('vacuous', () => {
-  expect(1 + 1).toBe(2);
+const execa = require('execa');
+const tempy = require('tempy');
+const cpy = require('cpy');
+const path = require('path');
+const packageJson = require('../package');
+const globby = require('globby');
+const fs = require('fs');
+const _ = require('lodash');
+
+test('git-find-files-not-modified-on-feature-branch', async () => {
+  const testDir = await tempy.dir();
+  await cpy(path.join('__fixtures__', '**', '*.*'), testDir, {dot: true});
+  await execa('mv', path.join(testDir, 'git'), path.join(testDir, '.git'));
+
+  const binPath = path.resolve(__dirname, '..', packageJson.bin['git-find-files-not-modified-on-feature-branch']);
+
+  await execa(binPath, [
+    '--compareBranch', 'compare',
+    '--baseBranch', 'base',
+    '--dry', 'false'
+  ], {cwd: testDir});
+
+  const filePaths = await globby(path.join(testDir, '**', '*.*'));
+  const fileContents = _(filePaths)
+    .map(filePath => [filePath, fs.readFileSync(filePath, 'utf8')])
+    .fromPairs()
+    .value();
+
+  expect(fileContents).toMatchSnapshot();
 });
